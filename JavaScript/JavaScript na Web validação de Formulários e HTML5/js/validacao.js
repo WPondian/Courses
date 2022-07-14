f361f54ad0118b35a271189ad574614fd08bc452
Willian Pondian <willian.pondian@outlook.com>
2022-07-13 22:32:46 -0300

    customizing the error message to be displayed directly in the html, validating the type of error directly from the element

--- JavaScript/JavaScript na Web validação de Formulários e HTML5/js/validacao.js
+++ JavaScript/JavaScript na Web validação de Formulários e HTML5/js/validacao.js
@@ -3,12 +3,59 @@ export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
 
     if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+        validadores[tipoDeInput](input);
+    }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').textContent = '';
+    }else{
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').textContent = mostraMensagemDeErro(tipoDeInput, input);
+        console.log(input.parentElement.querySelector('.input-mensagem-erro'))
     }
 }
 
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError',
+
+]
+
+const mensagensDeErro = {
+    nome:{
+        valueMissing: 'O campo nome não pode estar vazio!'
+    },
+    email:{
+        valueMissing: 'O campo email não pode estar vazio!',
+        typeMismatch: 'O email digitado não é valido!',
+    },
+    senha:{
+        valueMissing: 'O campo senha não pode estar vazio!',
+        patternMismatch: 'A senha deve conter entre seis a doze caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos!',
+    },
+    dataNascimento:{
+        customError:'Você deve ser maior que 18 anos para se cadastrar!',
+        valueMissing: 'O campo de data de nascimento não pode estar vazio!'
+    },
+};
+
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input){
+    let mensagem = '';
+
+    tiposDeErro.forEach(erro =>{
+        if(input.validity[erro]){
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+
+    return mensagem
 }
 
 function validaDataNascimento(input) {
