766469fce52b44c4ea4761471dea98c5a4da4521
WPondian <willian.pondian@outlook.com>
2022-07-18 12:55:26 -0300

    adding events on enter and space keys (course completed)

--- JavaScript/JavaScript para Web Crie páginas dinâmicas/js/main.js
+++ JavaScript/JavaScript para Web Crie páginas dinâmicas/js/main.js
@@ -1,19 +1,34 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+ const elemento = document.querySelector(seletorAudio);
+
+ if(elemento && elemento.localName === 'audio'){
+    elemento.play();
+ }else{
+    alert('Elemento não encontrado ou seletor não invalido!');
+ }
+
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 
-    tecla.onclick = function (){
-        tocaSom(idAudio);
-    };
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.remove("ativa");
+    }
+  };
 }
