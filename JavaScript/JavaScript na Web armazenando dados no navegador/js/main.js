a332620b1673de33445a2e764f03368f98ebc29a
WPondian <willian.pondian@outlook.com>
2022-08-02 23:45:25 -0300

    comparing if the item already exists and if it exists, update it

--- JavaScript/JavaScript na Web armazenando dados no navegador/js/main.js
+++ JavaScript/JavaScript na Web armazenando dados no navegador/js/main.js
@@ -17,9 +17,19 @@ form.addEventListener("submit", (event) => {
         "quantidade": quantidade.value,
     }
 
-    criaElemento(itemAtual);
 
-    itens.push(itemAtual);
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+
+    if(existe){
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+
+        itens[existe.id] = itemAtual
+    }else{
+        itemAtual.id = itens.length;
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -34,9 +44,13 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
-
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
