6afe04eb0b9d8a448eae231fe34160ec4f92aed1
WPondian <willian.pondian@outlook.com>
2022-09-09 18:00:40 -0300

    starting new course JavaScript

---
+++ JavaScript/JavaScript Avançado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,11 @@
+class NegociacaoController {
+
+    adiciona(event) {
+        event.preventDefault();
+        let inputData = document.querySelector('#data');
+        let inputQuantidade = document.querySelector('#quantidade');
+        let inputValor = document.querySelector('#valor');
+
+        console.log();
+    }
+}
