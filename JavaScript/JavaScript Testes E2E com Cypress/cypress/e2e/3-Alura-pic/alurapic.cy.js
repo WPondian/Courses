9f86b81b7c3efeb772f2dc35445c43424dca3b16
Willian Pondian <willian.pondian@outlook.com>
2022-10-05 00:12:18 -0300

    adding new tests for the validation form

--- JavaScript/JavaScript Testes E2E com Cypress/cypress/e2e/3-Alura-pic/alurapic.cy.js
+++ JavaScript/JavaScript Testes E2E com Cypress/cypress/e2e/3-Alura-pic/alurapic.cy.js
@@ -14,4 +14,27 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
+
+    it('verifica mensagem de e-mail invalido', () => {
+        cy.contains('a', 'Register now').click(); 
+        cy.contains('button', 'Register').click(); 
+        cy.get('input[formcontrolname="email"]').type('Jacqueline'); 
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click(); 
+        cy.contains('button', 'Register').click(); 
+        cy.get('input[formcontrolname="password"]').type('123'); 
+        cy.contains('button', 'Register').click(); 
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click(); 
+        cy.contains('button', 'Register').click(); 
+        cy.get('input[formcontrolname="userName"]').type('TESTANDO'); 
+        cy.contains('button', 'Register').click(); 
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+    })
 })
