8ffd3f516bb3ea628037d03ddb211df13ae4a4ad
WPondian <willian.pondian@outlook.com>
2023-01-17 22:39:45 -0300

    receiving response from the fetch function and handling the return with the then(), catch() and finnaly() method

--- JavaScript/js-consumindo-e-tratando-dados-api/script.js
+++ JavaScript/js-consumindo-e-tratando-dados-api/script.js
@@ -1,3 +1,15 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then((retorno)=> {
+    
+    if(retorno.erro){
+        throw Error('Esse cep não existe!')
+    }else{
+        console.log(retorno)
+    }
+
+})
+.catch(erro => console.error(erro))
+.finally(mensagem => console.log("procesamento concluido"));
 
 console.log(consultaCEP);
