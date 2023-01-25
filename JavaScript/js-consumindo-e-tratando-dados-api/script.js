8dffc4c62b40b03369ed98d2fcb470eb583f128e
WPondian <willian.pondian@outlook.com>
2023-01-25 09:15:32 -0300

    finish course

--- JavaScript/js-consumindo-e-tratando-dados-api/script.js
+++ JavaScript/js-consumindo-e-tratando-dados-api/script.js
@@ -1,19 +1,36 @@
 async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    
     try {
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let responseConvertido = await consultaCEP.json();
-        if(responseConvertido.erro){
+        if (responseConvertido.erro) {
             throw Error('CEP não existente!');
         }
-        console.log(responseConvertido);
+
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('logradouro');
+        let estado = document.getElementById('estado');
+
+        cidade.value = responseConvertido.localidade;
+        logradouro.value = responseConvertido.logradouro;
+        estado.value = responseConvertido.uf;
+
         return responseConvertido
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>Cep inválido. Tente novamente!</p>`
         console.log(erro);
     }
 }
 
 
 let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => {
+    if(cep.value){
+        buscaEndereco(cep.value);
+    }
+})
 
 // consulta varios ceps e depois com Promisse.all ele converte as promisses retornadas das consultas feitas.
 // let ceps = ['01001000','01001001'];
