f08f11231c5c5e4ccb6b5d070c4dc9d23140a6fc
WPondian <willian.pondian@outlook.com>
2024-04-24 21:31:27 -0300

    criando models

---
+++ TypeScript/typescript-evoluindo-seu-javascript/app/models/negociacoes.ts
@@ -0,0 +1,16 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao): void {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): ReadonlyArray<Negociacao> {
+        return this.negociacoes;
+    }
+}
+
+const negociacoes = new Negociacoes();
+negociacoes.lista()
